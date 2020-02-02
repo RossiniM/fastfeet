fd83e62c52c80cc8add6a866b61f01e8edeeb4b7
Rossini <rossini1408@gmail.com>
2020-02-02 10:27:10 -0300

    configure sequelize and environment variables

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import dotenv from 'dotenv';
+
+const env = dotenv.config();
+// eslint-disable-next-line import/first
 import app from './app';
 
-const port = 3333;
-
+const port = process.env.PORT || 5000;
+if (env.error) throw env.error;
 app.listen(port, () => console.log(`I 'am listening at port: ${port}`));
