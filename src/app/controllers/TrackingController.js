814936ba6f43e284181dcb78d8e0679735d341b0
Rossini <rossini1408@gmail.com>
2020-03-07 15:56:32 -0300

    - add file middleware - refac routes - create TrackingController

--- src/app/controllers/TrackingController.js
+++ src/app/controllers/TrackingController.js
@@ -1,6 +1,24 @@
 import { Op } from 'sequelize';
+import {
+  startOfHour,
+  parseISO,
+  isBefore,
+  isAfter,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  subHougetrs,
+} from 'date-fns';
 import Deliveryman from '../models/Deliveryman';
 import Order from '../models/Order';
+import fileController from './FileController';
+
+const isAllowedTime = (openHours, closedHours) => {
+  console.log('open', openHours, 'closed', closedHours);
+  console.log(new Date());
+  return isAfter(new Date(), openHours) && isBefore(new Date(), closedHours);
+};
 
 class TrackingController {
   async deliveredOrders(req, res) {
@@ -31,5 +49,61 @@ class TrackingController {
     });
     return res.status(200).json({ orders });
   }
+
+  async update(req, res) {
+    const { deliveryman_id, order_id } = req.query;
+
+    if (req.file) {
+      try {
+        const { fileId } = await fileController.store(req, res);
+        const order = await Order.update(
+          {
+            end_date: new Date(),
+            signature_id: fileId,
+          },
+          {
+            where: {
+              id: order_id,
+            },
+          }
+        );
+        return res.status(200).json({ order });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    const openHours = setHours(setMinutes(setSeconds(new Date(), 0), 0), 0);
+    const closedHours = setHours(setMinutes(setSeconds(new Date(), 0), 0), 18);
+
+    if (!isAllowedTime(openHours, closedHours))
+      return res
+        .status(400)
+        .json({ error: 'It is not alllowed to retrieve order at this time' });
+
+    const numberRetrieve = await Order.findAndCountAll({
+      where: {
+        deliveryman_id,
+        end_date: {
+          [Op.between]: [openHours, closedHours],
+        },
+      },
+    });
+    if (numberRetrieve > 5)
+      return res.status(400).json({
+        error: 'It is not alllowed to retrieve more than 5 orders at day',
+      });
+    const order = await Order.update(
+      {
+        start_date: new Date(),
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    return res.status(200).json({ order });
+  }
 }
+
 export default new TrackingController();
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import userController from './app/controllers/UserController';
 import recipientsController from './app/controllers/RecipientsController';
 import deliveryManController from './app/controllers/DeliverymanController';
 import orderController from './app/controllers/OrderController';
 import trackingController from './app/controllers/TrackingController';
+import multerConfig from './config/multer';
 
 import userSession from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.status(200).json({ health: 'ok' });
@@ -16,21 +20,22 @@ routes.get('/', (req, res) => {
 
 routes.post('/session', userSession.store);
 
-routes.post('/user', authMiddleware, userController.store);
-
-routes.post('/recipient', authMiddleware, recipientsController.store);
-
-routes.get('/deliveryman', authMiddleware, deliveryManController.index);
-routes.post('/deliveryman', authMiddleware, deliveryManController.store);
 routes.get('/deliveryman/:id/delivered', trackingController.deliveredOrders);
 routes.get('/deliveryman/:id/assigned', trackingController.assignedOrder);
+routes.put('/files/order', upload.single('file'), trackingController.update);
 
-routes.put('/deliveryman/:id', authMiddleware, deliveryManController.update);
-routes.delete('/deliveryman/:id', authMiddleware, deliveryManController.delete);
+routes.use(authMiddleware);
 
-routes.get('/order', authMiddleware, orderController.index);
-routes.post('/order', authMiddleware, orderController.store);
-routes.put('/order/:id', authMiddleware, orderController.update);
+routes.post('/user', userController.store);
+routes.post('/recipient', recipientsController.store);
+routes.get('/deliveryman', deliveryManController.index);
+routes.post('/deliveryman', deliveryManController.store);
+routes.put('/deliveryman/:id', deliveryManController.update);
+routes.delete('/deliveryman/:id', deliveryManController.delete);
+
+routes.get('/order', orderController.index);
+routes.post('/order', orderController.store);
+
 routes.delete('/order/:id', authMiddleware, orderController.delete);
 
 export default routes;
